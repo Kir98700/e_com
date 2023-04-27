3be0a6b6b5bfb16f5269afdc04a318f10ed77aca
kirtan <kirtansavani0406@gmail.com>
2023-04-27 09:36:38 +0530

    kirtana

--- lib/Inviewproduct.dart
+++ lib/Inviewproduct.dart
@@ -20,119 +20,121 @@ class _InviewproductState extends State<Inviewproduct> {
         appBar: AppBar(backgroundColor: Colors.black,
           title: Text('${widget.temp[widget.index]['name']}'),
         ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(5),
-              height: 270,
-              width: double.infinity,
-              child: Image.network(
-                '${widget.temp[widget.index]['Images']}',
-                fit: BoxFit.cover,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(5),
+                height: 270,
+                width: double.infinity,
+                child: Image.network(
+                  '${widget.temp[widget.index]['Images']}',
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Spacer(),
+                        Text(
+                          '${widget.temp[widget.index]['name']}',
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Spacer(),
+                        Text(
+                          ' ${widget.temp[widget.index]['Description']}',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Text(
+                          'Price ',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Spacer(),
+                        Text(
+                          '${widget.temp[widget.index]['Price']}',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 50,
+                width: double.infinity,
+                color: Colors.grey,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.star, color: Colors.yellow),
+                    Icon(Icons.star, color: Colors.yellow),
+                    Icon(Icons.star, color: Colors.yellow),
+                    Icon(Icons.star, color: Colors.white),
+                    Icon(Icons.star, color: Colors.white),
+                    SizedBox(width: 10),
+                    Text('(4.0)'),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.remove),
+                      onPressed: () {
+                        setState(() {
+                          i--;
+                        });
+                      },
+                    ),
+                    SizedBox(width: 10),
+                    Text("${i}"),
+                    SizedBox(width: 10),
+                    IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: () {
+                        setState(() {
+                          i++;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Column(
                 children: [
-                  Row(
-                    children: [
-                      Spacer(),
-                      Text(
-                        '${widget.temp[widget.index]['name']}',
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Spacer(),
-                      Text(
-                        ' ${widget.temp[widget.index]['Description']}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Text(
-                        'Price ',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      Spacer(),
-                      Text(
-                        '${widget.temp[widget.index]['Price']}',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
+                  Container(
+                      height: 50,
+                      width: 150,
+                      color: Colors.grey,
+                      child: IconButton(
+                          onPressed: () {}, icon: Icon(Icons.shopping_cart))),
                 ],
               ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              height: 50,
-              width: double.infinity,
-              color: Colors.grey,
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.star, color: Colors.yellow),
-                  Icon(Icons.star, color: Colors.yellow),
-                  Icon(Icons.star, color: Colors.yellow),
-                  Icon(Icons.star, color: Colors.white),
-                  Icon(Icons.star, color: Colors.white),
-                  SizedBox(width: 10),
-                  Text('(4.0)'),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Row(mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.remove),
-                    onPressed: () {
-                      setState(() {
-                        i--;
-                      });
-                    },
-                  ),
-                  SizedBox(width: 10),
-                  Text("${i}"),
-                  SizedBox(width: 10),
-                  IconButton(
-                    icon: Icon(Icons.add),
-                    onPressed: () {
-                      setState(() {
-                        i++;
-                      });
-                    },
-                  ),
-                ],
-              ),
-            ),
-            Column(
-              children: [
-                Container(
-                    height: 50,
-                    width: 150,
-                    color: Colors.grey,
-                    child: IconButton(
-                        onPressed: () {}, icon: Icon(Icons.shopping_cart))),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
